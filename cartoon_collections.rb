4bca3052de7051bf5bd532a3419d3ebeb49f2aed
Karim Alomari <karim.alomari@gmail.com>
2020-07-28 17:03:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+  value = array.find{|i| i.length > 4}
+  value == nil ? false : true
 end
 
 def find_the_cheese# code an argument here
